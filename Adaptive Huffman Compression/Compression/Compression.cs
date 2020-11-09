13b3d77b3542e2b4ab5f4652a031160f4b82acc1
agoskomsija <agostvv@gmail.com>
2020-11-09 19:01:25 +0100

    compression decompression

---
+++ Adaptive Huffman Compression/Compression/Compression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compression
+{
+    class Compression
+    {
+        public static void HuffmanCompression()
+        {
+
+        }
+    }
+}
---
+++ Adaptive Huffman Compression/Compression/Decompression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compression
+{
+    class Decompression
+    {
+        public static void HuffmanDecompression()
+        {
+
+        }
+    }
+}
