023bde9e41d0825ff0068e299b6a21ae5e244e00
Sokol <stevaaa@elfak.rs>
2020-11-17 21:24:52 +0100

    Opet dodata kompresija

--- Adaptive Huffman Compression/Compression/Compression.cs
+++ Adaptive Huffman Compression/Compression/Compression.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BinaryBitLib;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -11,10 +12,31 @@ namespace Compression
     {
         public bool Execute(Stream stream, ProgressBar progressBar = null, string fileName = null, string path = null)
         {
-            HuffmanCodesGenerator generator = new HuffmanCodesGenerator();
-            List<KeyValuePair<char,List<byte>>> symbolCodeTable = generator.GetCodes(); 
+            table = new Table();
+            string fileNamee = @"C:\Users\Stefan\Desktop\test.bin";
+            using (StreamReader reader = new StreamReader(stream))
+            using (BinaryBitWriter binWriter = new BinaryBitWriter(new FileStream(fileNamee, FileMode.OpenOrCreate)))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                        WriteCode(line[i], binWriter);
+                }
+            }
 
             return true;
         }
+
+        #region Properties
+        Table table;
+        #endregion
+        #region Methods
+        private void WriteCode(char symbol, BinaryBitWriter writer)
+        {
+            writer.WriteBits(table.SearchBySymbol(symbol).ToArray());
+            table.IncrementFreq(symbol);
+        }
+        #endregion
     }
 }
